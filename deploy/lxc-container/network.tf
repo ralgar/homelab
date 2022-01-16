c17193d5cbc9a85e5c8e2d0e38083640c947b26c
basschaser <59636191+basschaser@users.noreply.github.com>
2022-01-16 12:50:30 -0800

    Terraform: Add Proxmox LXC container child module

---
+++ deploy/lxc-container/network.tf
@@ -0,0 +1,19 @@
+// DNS A Records (Forward Zone)
+resource "dns_a_record_set" "lxc-dns-a-record" {
+  zone = "${var.netDomain}."
+  name = var.hostname
+  addresses = [
+    "${cidrhost(var.netPrefix, var.netHostNums[var.countIndex])}"
+  ]
+  ttl = 3600
+  count = var.addToDns ? 1 : 0
+}
+
+// DNS Pointer Records (Reverse Zone)
+resource "dns_ptr_record" "lxc-dns-ptr-record" {
+  zone = "${join(".", reverse(slice(split(".", var.netPrefix), 0, 3)))}.in-addr.arpa."
+  name = var.netHostNums[var.countIndex]
+  ptr  = "${var.hostname}.${var.netDomain}."
+  ttl  = 3600
+  count = var.addToDns ? 1 : 0
+}
