382ec98005c3eb4ece17a01a7fbd56b42c7ee51f
basschaser <59636191+basschaser@users.noreply.github.com>
2022-01-16 12:51:36 -0800

    Terraform: Add nameservers to deployment

---
+++ deploy/nameservers.tf
@@ -0,0 +1,43 @@
+module "nameservers" {
+  // Guest Settings
+  source      = "./lxc-container"
+  hostname    = "ns${count.index + 1}"
+  title       = "Name Server #${count.index + 1 }"
+  description = <<-EOT
+    A recursive DNS server powered by BIND.
+  EOT
+  count       = length(var.guest_hostNums)
+  countIndex  = count.index
+  cores       = 1
+  memory      = 128
+  swap        = 128
+  rootSize    = 8
+  startOnBoot = true
+  addToDns    = false
+  tags        = [ "nameservers", "prod" ]
+
+  // Global Passthrough Variables
+  dnsHostNums      = var.dns_hostNums
+  dnsSecretKey     = var.dns_secretKey
+  dnsSecretType    = var.dns_secretType
+  pveHost          = var.pve_host
+  pveUsername      = var.pve_username
+  pvePassword      = var.pve_password
+  pveTlsInsecure   = var.pve_tlsInsecure
+  guestCtImage     = var.guest_ctImage
+  guestPubKeyFile  = var.guest_pubKeyFile
+  guestTargetNode  = var.guest_targetNode
+  guestStoragePool = var.guest_storagePool
+  guestVmTemplate  = var.guest_vmTemplate
+  netDnsHosts      = [
+    "${cidrhost(var.net_prefix, var.dns_hostNums[0])}",
+    "1.1.1.1",
+    "1.0.0.1"
+  ]
+  netDomain        = var.net_domain
+  netGateway       = var.net_gateway
+  netPrefix        = var.net_prefix
+
+  // Module Variables
+  netHostNums      = var.guest_hostNums
+}
