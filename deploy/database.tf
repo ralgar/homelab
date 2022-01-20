2eb9c1c1e0e8293e9d89c63d153261e5d58e61cc
basschaser <59636191+basschaser@users.noreply.github.com>
2022-01-19 21:22:38 -0800

    Add basic PostgreSQL deployment

---
+++ deploy/database.tf
@@ -0,0 +1,43 @@
+module "postgresql" {
+  // Guest Settings
+  source      = "./lxc-container"
+  hostname    = "postgres"
+  title       = "PostgreSQL"
+  description = <<-EOT
+    The database for homelab services.
+  EOT
+  count       = 1
+  countIndex  = count.index
+  cores       = 1
+  memory      = 256
+  swap        = 256
+  rootSize    = 20
+  startOnBoot = true
+  addToDns    = true
+  tags        = [ "db", "prod" ]
+
+  // Global Variables
+  dnsHostNums      = var.dns_hostNums
+  dnsSecretKey     = var.dns_secretKey
+  dnsSecretType    = var.dns_secretType
+  pveHost          = var.pve_host
+  pveUsername      = var.pve_username
+  pvePassword      = var.pve_password
+  pveTlsInsecure   = var.pve_tlsInsecure
+  guestCtImage     = var.guest_ctImage
+  guestPubKeyFile  = var.guest_pubKeyFile
+  guestTargetNode  = var.guest_targetNode
+  guestStoragePool = var.guest_storagePool
+  guestVmTemplate  = var.guest_vmTemplate
+  netDnsHosts      = [
+    "${cidrhost(var.net_prefix, var.dns_hostNums[0])}",
+    "${cidrhost(var.net_prefix, var.dns_hostNums[1])}",
+    "1.1.1.1"
+  ]
+  netDomain        = var.net_domain
+  netGateway       = var.net_gateway
+  netPrefix        = var.net_prefix
+
+  // Module Variables
+  netHostNums      = var.guest_hostNums
+}
