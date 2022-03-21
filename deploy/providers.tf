499da5af785e72464afd8d3d00a60063046919ab
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-03-20 19:42:47 -0700

    Impr: Simplify configuration
    Variables for the Terraform module are now set automatically from the
    dynamic inventory configuration. This was done by including the dynamic
    inventory configuration as a variable when using the `terraform-deploy`
    role.

--- deploy/providers.tf
+++ deploy/providers.tf
@@ -21,8 +21,8 @@ provider "dns" {
 }
 
 provider "proxmox" {
-  pm_api_url  = "https://${var.pve_host}:8006/api2/json"
-  pm_user     = "${var.pve_username}@pam"
-  pm_password = var.pve_password
+  pm_api_url      = "${var.pve_host}/api2/json"
+  pm_user         = "${var.pve_username}"
+  pm_password     = var.pve_password
   pm_tls_insecure = var.pve_tlsInsecure
 }
