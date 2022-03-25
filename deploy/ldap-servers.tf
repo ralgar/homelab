5bd6f1212067195fa5c9804084983d4090c96251
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-03-24 17:28:03 -0700

    Squashed commit of the following:
    commit 654beb87309c337536a7edd38eec3d78d48ed1e3
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 17:18:04 2022 -0700
    
        Merge LDAP playbook into infrastructure playbook
    
    commit f6f3da6d50df9de30f10450a1d001302e7a7840e
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 17:16:30 2022 -0700
    
        Edit playbook task titles for infrastructure.yml
    
    commit 231a9cb956477f2ef863f70b24d233d18c8f123f
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 17:16:12 2022 -0700
    
        Change LDAP server hostNum
    
    commit 9820e803250be2c5c865f3e7d5b193e78c64eaf0
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 17:12:02 2022 -0700
    
        Fix: Flush handlers after enabling plugins
    
        This fixes group membership not being picked up for the initial users
        and groups.
    
    commit b0e6cf4feb4055ba96dd50c1a5483a6bb438a065
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 16:35:40 2022 -0700
    
        Rename LDAP playbook
    
    commit 5da9950d4599734d34589dd77fb4712ac80cf71f
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 16:35:15 2022 -0700
    
        Fix: Hostname and count for LDAP server
    
    commit 05e5408a6348c60a13337e4491e66640acfb4db0
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 16:18:04 2022 -0700
    
        Update ldap-servers.yml
    
    commit d13a73f5ed212c4b5942911d2f9bb82cf13b9c86
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:29:16 2022 -0700
    
        Fix: Path for LDAP TLS cert check
    
    commit 2ff700149672a981529f367c13d8be0d7a8f6cae
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:21:13 2022 -0700
    
        Impr: Add vars/hosts.proxmox.yml to ldap-servers playbook
    
    commit 7e7aac8e1c2e5b2840e676d85a65884b2a46c3b0
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:20:39 2022 -0700
    
        Update LDAP init template
    
    commit d363f574642676e80dfa3e649c05b89e18206754
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:20:18 2022 -0700
    
        Update LDAP main.yml
    
    commit d7e5e6cb0be49f7f9f32ed7822349d7f1f3caab4
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:19:55 2022 -0700
    
        Add default users and groups
    
    commit 69bd2a13071e31747f835a2724dae0a21a87f85b
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:18:57 2022 -0700
    
        Fix: Add entries for attribute uniqueness plugins
    
    commit 64ed3c69ce859b8b46ce6f2292e9c97ce05b4ea1
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:17:08 2022 -0700
    
        Impr: Remove init config file after import
    
    commit daa6065ec190d4dcdcdb9028f727fa5c8e8865f3
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Mar 24 15:15:25 2022 -0700
    
        Fix and improve TLS configuration
    
    commit ed65648974daa9f0822bbe91a02286dd6b834d1a
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Feb 10 14:11:34 2022 -0800
    
        Fix: LDAP server count
    
        Only spawn 1 LDAP server because that's all we need for now.
    
    commit 16f24df15fd6d136d26936c267cde17a1bccc74b
    Author: Ryan Algar <59636191+ralgar@users.noreply.github.com>
    Date:   Thu Feb 10 14:03:08 2022 -0800
    
        Auth: Add initial LDAP implementation
    
        - Essential plugins configured
        - Enhanced security over defaults
        - TLS configured

---
+++ deploy/ldap-servers.tf
@@ -0,0 +1,43 @@
+module "ldap-server" {
+  // Guest Settings
+  source      = "./lxc-container"
+  hostname    = "ldap"
+  title       = "LDAP Server"
+  description = <<-EOT
+    Directory server powered by 389DS.
+  EOT
+  count       = 1
+  countIndex  = count.index
+  cores       = 1
+  memory      = 256
+  swap        = 256
+  rootSize    = 8
+  startOnBoot = true
+  addToDns    = true
+  tags        = [ "ldap" ]
+
+  // Global Variables
+  dnsHostNums      = var.dns_hostNums
+  dnsSecretKey     = var.dns_secretKey
+  dnsSecretType    = var.dns_secretType
+  pveHost          = var.pve_host
+  pveUsername      = var.pve_username
+  pvePassword      = var.pve_password
+  pveTlsInsecure   = var.pve_tlsInsecure
+  guestCtImage     = var.guest_ctImage
+  guestPubKeyFile  = var.guest_pubKeyFile
+  guestTargetNode  = var.guest_targetNode
+  guestStoragePool = var.guest_storagePool
+  guestVmTemplate  = var.guest_vmTemplate
+  netDnsHosts      = [
+    "${cidrhost(var.net_prefix, var.dns_hostNums[0])}",
+    "${cidrhost(var.net_prefix, var.dns_hostNums[1])}",
+    "1.1.1.1"
+  ]
+  netDomain        = var.net_domain
+  netGateway       = var.net_gateway
+  netPrefix        = var.net_prefix
+
+  // Module Variables
+  netHostNums      = var.guest_hostNums
+}
