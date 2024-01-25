69a2525792117b7fe28b0a0485e8ad5a9e2dad51
Ryan Algar <12025308-ralgar@users.noreply.gitlab.com>
2024-01-25 21:22:44 +0000

    fix(infra): GCP bucket parameters
    Fixes the following:
    
    - Ensure GCP bucket adheres to free tier requirements (use REGIONAL storage class).
    - Enforce public access prevention.
    
    MR: https://gitlab.com/ralgar/homelab/-/merge_requests/42

--- infra/modules/gcp/bucket/main.tf
+++ infra/modules/gcp/bucket/main.tf
@@ -7,11 +7,15 @@ resource "random_string" "bucket_id" {
 }
 
 resource "google_storage_bucket" "default" {
-  name     = "${var.name_prefix}-${random_string.bucket_id.result}"
-  location = "US-WEST1"  # Free tier must use US-WEST1, US-CENTRAL1, or US-EAST1
+  name          = "${var.name_prefix}-${random_string.bucket_id.result}"
+  location      = "US-WEST1"  # Free tier must use US-WEST1, US-CENTRAL1, or US-EAST1
+  storage_class = "REGIONAL"  # Free tier must use REGIONAL storage class
 
   # When destroying a bucket, destroy all contained objects.
   force_destroy = true
+
+  # Ensure no public access
+  public_access_prevention = "enforced"
 
   uniform_bucket_level_access = true
 
