6f1d581a2dc1f1be017b7c0515cfcbd5398e0063
Ryan Algar <12025308-ralgar@users.noreply.gitlab.com>
2024-08-20 19:43:51 +0000

    feat(metal): deploy Manila service
    This change implements a fully-functional deployment of the Manila service, with one small caveat for now - we've had to temporarily allow local storage, so that Manila can successfully spawn an instance.
    
    Closes #28
    
    Opens #29
    
    MR: https://gitlab.com/ralgar/homelab/-/merge_requests/121

---
+++ metal/tests/function/neutron.tf
@@ -0,0 +1,38 @@
+resource "openstack_networking_network_v2" "test_network_1" {
+  name           = "test_network_1"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "test_subnet_1" {
+  name       = "test_subnet_1"
+  cidr       = "10.254.254.0/24"
+  ip_version = 4
+  network_id = openstack_networking_network_v2.test_network_1.id
+}
+
+// Create the router between the internal and external networks
+data "openstack_networking_network_v2" "external" {
+  name = "public"
+}
+
+data "openstack_networking_subnet_v2" "external" {
+  name = "public"
+}
+
+resource "openstack_networking_router_v2" "internal" {
+  name                = "test_router_1"
+  admin_state_up      = true
+  external_network_id = data.openstack_networking_network_v2.external.id
+}
+
+resource "openstack_networking_router_interface_v2" "internal" {
+  router_id = openstack_networking_router_v2.internal.id
+  subnet_id = openstack_networking_subnet_v2.test_subnet_1.id
+}
+
+resource "openstack_networking_router_route_v2" "internal_default" {
+  depends_on       = [openstack_networking_router_interface_v2.internal]
+  router_id        = openstack_networking_router_v2.internal.id
+  destination_cidr = "0.0.0.0/0"
+  next_hop         = data.openstack_networking_subnet_v2.external.gateway_ip
+}
