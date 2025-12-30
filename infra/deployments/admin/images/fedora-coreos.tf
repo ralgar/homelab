51d61daa1942fdc0063c52090ff45949478a40c9
Ryan Algar <12025308-ralgar@users.noreply.gitlab.com>
2025-12-30 14:37:56 -0800

    chore(infra/admin): add latest FCOS image
    MR: https://gitlab.com/ralgar/homelab/-/merge_requests/240

--- infra/deployments/admin/images/fedora-coreos.tf
+++ infra/deployments/admin/images/fedora-coreos.tf
@@ -26,3 +26,32 @@ resource "openstack_images_image_v2" "fcos_41_2025_03_15" {
     ]
   }
 }
+
+resource "openstack_images_image_v2" "fcos_43_2025_11_20" {
+  name             = "Fedora CoreOS 43 (2025.11.20)"
+  image_source_url = "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/43.20251120.3.0/x86_64/fedora-coreos-43.20251120.3.0-openstack.x86_64.qcow2.xz"
+
+  container_format = "bare"
+  disk_format      = "qcow2"
+  decompress       = true
+  min_disk_gb      = 10
+  min_ram_mb       = 1536
+  visibility       = var.environment == "prod" ? "public" : "private"
+
+  properties = {
+    architecture  = "x86_64"
+    os_type       = "linux"
+    os_distro     = "fedora-coreos"
+    os_version    = "43"
+    os_admin_user = "core"
+
+    cinder_img_volume_type = "PREMIUM"
+  }
+
+  // Ignore when Glance converts the image to RAW format
+  lifecycle {
+    ignore_changes = [
+      disk_format
+    ]
+  }
+}
